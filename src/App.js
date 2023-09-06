0fb9a2fee4821c37d91166e1b68f45d37ec55368
JussiMo <n2moju00@students.oamk.fi>
2023-09-06 21:57:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
   //const numbers = new Array(24).fill(null).map((_,i) => i+1)
-
+ //mikähomma?
   const calculate = () => {
     let alko = 0
     let litres = bottle * 0.33
